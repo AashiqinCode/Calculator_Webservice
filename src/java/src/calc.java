e90af220d72af5a620f826ae363b384f62517a4d
Aashiq Ahmed M <48018540+aashiqahmed786@users.noreply.github.com>
2020-01-03 11:16:13 +0530

    First initial commit!

---
+++ src/java/src/calc.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Aashiq
+ */
+@WebService(serviceName = "calc")
+public class calc {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+        //TODO write your implementation code here:
+        
+        int c=a+b;
+        return c;
+    }
+}
